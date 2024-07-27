40366912b47754666684e19d970670a5915ce0b3
Isx <ismael.isx21@gmail.com>
2024-07-26 18:30:21 -0600

    Re organize files done

--- Leetcode/26 removeDuplicates.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def __init__(self, nums: list[int]) -> int:
-        nums [:] = list(set(nums))
-        print(nums)
-
-
-prueba = Solution([1,1,2])
--- Leetcode/hola.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s =  "".join(filter(str.isalnum, s)).lower()
-        if s[::] == s[::-1]:
-            return True
-        else:
-            return False
